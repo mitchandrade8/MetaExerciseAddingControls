49fc83322410bac69a4992012345dda5a28eecb6
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-03 15:31:05 -0600

    Initial Commit

---
+++ MetaExerciseAddingControls/MetaExerciseAddingControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaExerciseAddingControlsApp.swift
+//  MetaExerciseAddingControls
+//
+//  Created by Mitch Andrade on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MetaExerciseAddingControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
